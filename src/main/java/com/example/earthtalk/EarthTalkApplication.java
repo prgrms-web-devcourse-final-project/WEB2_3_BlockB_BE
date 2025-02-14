16af8c5691050df15980893d1a82b6d859f8fb4e
ohdeng02 <sujin5792@naver.com>
2025-02-14 15:14:50 +0900

    init project

---
+++ src/main/java/com/example/earthtalk/EarthTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.earthtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarthTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/earthtalk/Web23BlockBBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.earthtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web23BlockBBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
