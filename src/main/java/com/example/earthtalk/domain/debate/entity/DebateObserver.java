3502fc1027e92b1924fcfffa8c2e7325980dbc8f
JinWoong <slsl03264@naver.com>
2025-02-17 12:23:38 +0900

    feat: 관객 정보를 확인하기 위한 DebateObserver Entity 추가

---
+++ src/main/java/com/example/earthtalk/domain/debate/entity/DebateObserver.java
@@ -0,0 +1,43 @@
+package com.example.earthtalk.domain.debate.entity;
+
+import com.example.earthtalk.domain.user.entity.User;
+import com.example.earthtalk.global.baseTime.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity(name = "debate_observers")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class DebateObserver extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "debate_id", nullable = false)
+    private Debate debate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private boolean flag;
+
+}
