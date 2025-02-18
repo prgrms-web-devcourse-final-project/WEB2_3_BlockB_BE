e5ec642721ac62354573d90d993dab40dc594822
박현욱 <nic991002@gmail.com>
2025-02-18 14:48:15 +0900

    feat : 대륙 수정 및 뉴스타입 추가
    - 개발 시작에 앞서 뉴스타입과 대륙 타입을 확정지었습니다.

--- src/main/java/com/example/earthtalk/domain/news/entity/NewsType.java
+++ src/main/java/com/example/earthtalk/domain/news/entity/NewsType.java
@@ -4,6 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum NewsType {
-    연합뉴스,
-    경향신문; //예시 신문사
+    SEOUL("서울신문"),
+    KHAN("경향신문"),
+    HANKYUNG("한국경제"),
+    DONGA("동아일보"),
+    HANKOOK("한국일보"),
+    SEGYE("세계일보");
+
+    private final String value;
+
+    NewsType(String value) {
+        this.value = value;
+    }
 }
