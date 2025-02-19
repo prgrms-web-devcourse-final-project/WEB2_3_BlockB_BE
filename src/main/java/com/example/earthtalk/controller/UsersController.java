2931cd8c765f400c8b604576e5d506c77087e377
jeongeun <deerellen2001@gmail.com>
2025-02-19 15:59:47 +0900

    refactor: JPQL을 QueryDSL로 변경

--- src/main/java/com/example/earthtalk/controller/UsersController.java
+++ src/main/java/com/example/earthtalk/controller/UsersController.java
@@ -1,15 +1,10 @@
 package com.example.earthtalk.controller;
 
-import com.example.earthtalk.domain.news.entity.News;
-import com.example.earthtalk.domain.news.service.NewsService;
-import com.example.earthtalk.domain.user.entity.User;
 import com.example.earthtalk.domain.user.service.UserService;
-import com.example.earthtalk.domain.user.userDto.UserInfoDto;
+import com.example.earthtalk.domain.user.dto.response.UserInfoResponse;
 import com.example.earthtalk.global.response.ApiResponse;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +19,8 @@ public class UsersController {
     private final UserService userService;
 
     @GetMapping("/followers")
-    public ResponseEntity<ApiResponse<Object>> getUserInfo() {
-        List<UserInfoDto> response = userService.getPopularUsersInfo();
+    public ResponseEntity<ApiResponse<List<UserInfoResponse>>> getUserInfo() {
+        List<UserInfoResponse> response = userService.getPopularUsersInfo();
         return ResponseEntity.ok().body(ApiResponse.createSuccess(response));
     }
 }
--- src/main/java/com/example/earthtalk/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/earthtalk/domain/user/repository/UserRepository.java
@@ -8,16 +8,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    @Query("SELECT u.id AS userId, " +
-        "COALESCE(COUNT(DISTINCT f1.follower.id), 0) AS totalFollowers, " +
-        "COALESCE(COUNT(DISTINCT f2.followee.id), 0) AS totalFollowees " +
-        "FROM users u " +
-        "LEFT JOIN follows f1 ON u.id = f1.followee.id " +
-        "LEFT JOIN follows f2 ON u.id = f2.follower.id " +
-        "GROUP BY u.id " +
-        "ORDER BY totalFollowers DESC")
-    List<Object[]> getFollowerFolloweeCount();
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 
 }
