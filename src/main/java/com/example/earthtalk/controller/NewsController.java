e02182e0e392b4b50e3555ab430a010cff36285a
박현욱 <nic991002@gmail.com>
2025-02-24 11:49:42 +0900

    feat: 스웨거 설명 추가

--- src/main/java/com/example/earthtalk/controller/NewsController.java
+++ src/main/java/com/example/earthtalk/controller/NewsController.java
@@ -6,6 +6,7 @@ import com.example.earthtalk.domain.news.entity.News;
 import com.example.earthtalk.domain.news.service.NewsDataService;
 import com.example.earthtalk.domain.news.service.NewsService;
 import com.example.earthtalk.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -28,9 +29,9 @@ public class NewsController {
     private final NewsService newsService;
     private final NewsDataService newsDataService;
 
-    /***
-     * 예시 API
-     */
+
+    @Operation(summary = "모든 뉴스 조회 및 필터링용 API 입니다.",
+    description = "스크롤 해서 새로운 뉴스를 요청할 때 cursor 에 현재까지 로드된 뉴스 중 마지막 뉴스의 id를 주시면 됩니다.")
     @GetMapping
     public ResponseEntity<ApiResponse<Object>> getNewsList(
         @RequestParam(value = "continent", required = false) String continent,
@@ -42,6 +43,8 @@ public class NewsController {
         return ResponseEntity.ok().body(ApiResponse.createSuccess(data));
     }
 
+    @Operation(summary = "뉴스 상세보기 API입니다.",
+    description = "사용자 id를 요청에 포함시키면 사용자가 해당 뉴스에 좋아요,북마크를 이미 눌렀는지에 대한 정보를 추가로 반환합니다.")
     @GetMapping("/{newsId}")
     public ResponseEntity<ApiResponse<Object>> getNewsById(
         @PathVariable("newsId") Long newsId,
@@ -50,12 +53,14 @@ public class NewsController {
         return ResponseEntity.ok().body(ApiResponse.createSuccess(data));
     }
 
+    @Operation(summary = "뉴스 Top10 API 입니다.", description = "뉴스 조회하기와 같은 형식의 데이터를 반환합니다.")
     @GetMapping("/ranking")
     public ResponseEntity<ApiResponse<Object>> getNewsRanking() {
         List<NewsListDTO> data = newsDataService.getNewsRanking();
         return ResponseEntity.ok().body(ApiResponse.createSuccess(data));
     }
 
+    @Operation(summary = "좋아요 추가 API 입니다.", description = "뉴스아이디와 사용자 아이디를 기반으로 좋아요를 생성합니다.")
     @PostMapping("/{newsId}/like")
     public ResponseEntity<ApiResponse<Object>> likeNews(
         @PathVariable("newsId") Long newsId,
@@ -64,6 +69,7 @@ public class NewsController {
         return ResponseEntity.ok().body(ApiResponse.createSuccessWithNoData());
     }
 
+    @Operation(summary = "좋아요 취소 API 입니다.", description = "생성돼있던 좋아요를 삭제합니다.")
     @DeleteMapping("/{newsId}/like")
     public ResponseEntity<ApiResponse<Object>> unlikeNews(
         @PathVariable("newsId") Long newsId,
@@ -72,6 +78,7 @@ public class NewsController {
         return ResponseEntity.ok().body(ApiResponse.createSuccessWithNoData());
     }
 
+    @Operation(summary = "북마크 추가 API 입니다.", description = "뉴스아이디와 사용자 아이디를 기반으로 북마크를 추가합니다.")
     @PostMapping("/{newsId}/bookmark")
     public ResponseEntity<ApiResponse<Object>> bookmarkNews(
         @PathVariable("newsId") Long newsId,
@@ -80,6 +87,7 @@ public class NewsController {
         return ResponseEntity.ok().body(ApiResponse.createSuccessWithNoData());
     }
 
+    @Operation(summary = "북마크 취소 API 입니다.", description = "생성돼있던 북마크를 삭제합니다.")
     @DeleteMapping("/{newsId}/bookmark")
     public ResponseEntity<ApiResponse<Object>> unmarkNews(
         @PathVariable("newsId") Long newsId,
