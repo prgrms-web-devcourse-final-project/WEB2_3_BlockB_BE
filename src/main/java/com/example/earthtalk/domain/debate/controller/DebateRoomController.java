e218fe93e506ad5027474d751d6ec119a3471b02
JinWoong <slsl03264@naver.com>
2025-02-24 21:48:24 +0900

    fix: 잘못된 문법 수정

--- src/main/java/com/example/earthtalk/domain/debate/controller/DebateRoomController.java
+++ src/main/java/com/example/earthtalk/domain/debate/controller/DebateRoomController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +28,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-@RestController("/api/debates")
+@RestController
+@RequestMapping("/api/debates")
 @RequiredArgsConstructor
 @Tag(name = "debateRoom", description = "채팅방 관련 API")
 public class DebateRoomController {
