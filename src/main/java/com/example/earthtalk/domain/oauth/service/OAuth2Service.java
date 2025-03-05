8c855f42eb903fb42cd7212035540e7159a5cf35
ohdeng02 <sujin5792@naver.com>
2025-03-05 12:18:41 +0900

    refactor: 인증 로직 수정 & transactional 처리 추가

--- src/main/java/com/example/earthtalk/domain/oauth/service/OAuth2Service.java
+++ src/main/java/com/example/earthtalk/domain/oauth/service/OAuth2Service.java
@@ -12,12 +12,14 @@ import com.example.earthtalk.domain.user.entity.User;
 import com.example.earthtalk.domain.user.repository.UserRepository;
 import com.example.earthtalk.global.exception.ErrorCode;
 import com.example.earthtalk.global.exception.NotFoundException;
+import com.example.earthtalk.global.exception.IllegalArgumentException;
 import com.example.earthtalk.global.exception.OAuth2AuthenticationException;
 import com.example.earthtalk.domain.oauth.dto.response.TokenResponse;
 import com.example.earthtalk.global.security.util.JwtTokenProvider;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Map;
@@ -52,12 +54,12 @@ public class OAuth2Service {
         OAuthClient oAuthClient = oauthClients.get(provider);
 
         log.info("call OAuthService.getIdTokenFromAuthCode[authCode: {}]", authCode);
-        String accessToken = oAuthClient.getAccessTokenFromAuthCode(clientRegistration, authCode);
-        return getResourceFromToken(request, response, oAuthClient, accessToken, clientRegistration);
+        TokenResponse.GetToken tokens = oAuthClient.getAccessTokenFromAuthCode(clientRegistration, authCode);
+        return getResourceFromToken(request, response, oAuthClient, tokens, clientRegistration);
     }
 
     public TokenResponse.GetOauth getResourceFromToken(HttpServletRequest request, HttpServletResponse response,
-        OAuthClient oAuthClient, String accessToken, ClientRegistration clientRegistration) {
+        OAuthClient oAuthClient, TokenResponse.GetToken tokens, ClientRegistration clientRegistration) {
 
         // 소셜 타입 결정
         SocialType socialType = getSocialType(clientRegistration.getClientName());
@@ -68,7 +70,7 @@ public class OAuth2Service {
             .getUserNameAttributeName();
 
         // Access Token으로 사용자 정보 가져오기
-        Map<String, Object> userAttributes = oAuthClient.getProfileFromAccessToken(clientRegistration, accessToken);
+        Map<String, Object> userAttributes = oAuthClient.getProfileFromAccessToken(clientRegistration, tokens.accessToken());
         log.info("userAttributes: {}", userAttributes);
 
         OAuthAttributes extractAttributes = OAuthAttributes.of(
@@ -77,64 +79,94 @@ public class OAuth2Service {
             userAttributes
         );
 
-        User user = getOrSaveUser(extractAttributes, socialType); // 유저 정보 저장 또는 업데이트
+        User user = getOrSaveUser(extractAttributes, socialType, tokens); // 유저 정보 저장 또는 업데이트
         CustomOAuth2User oAuth2User = setAuthenticationContext(user, userAttributes, extractAttributes.getNameAttributeKey()); // 인증 객체 생성 및 SecurityContext에 설정
 
         TokenResponse.GetToken tokenResponse = jwtTokenProvider.generateAllTokens(oAuth2User, new Date());
         saveRefreshToken(oAuth2User, tokenResponse.refreshToken());
 
-        String userNickname = user.getNickname();
-        if (user.getRole().equals(Role.ROLE_GUEST)) {
-            userNickname = user.getNickname().split("_")[1];
-        }
-
-        // 프론트엔드로 JWT, 닉네임, 프로필 포함하여 반환
+        // 프론트엔드로 JWT, 프로필 포함하여 반환
         // TODO: token은 cookie로 변경, 프로필 정보는 API 따로 호출 고려
         return TokenResponse.GetOauth.from(
             user.getId(),
             tokenResponse.accessToken(),
             tokenResponse.refreshToken(),
-            userNickname, //nickname split해서 주기
+            user.getNickname(),
             user.getProfileUrl(),
             user.getRole().toString());
     }
 
     // AccessToken 만료시, AccessToken, RefreshToken 재발급
-    public TokenResponse.GetToken getReissue(String bearerToken) {
+    @Transactional
+    public TokenResponse.GetToken getReissue(String refreshToken) {
         // TODO: Refresh Token 만료기간 관리 -> Redis 관리
-        String refreshToken = jwtTokenProvider.parseBearerToken(bearerToken);
+        RefreshToken originToken = refreshTokenRepository.findByToken(refreshToken)
+            .orElseThrow(() -> new NotFoundException(ErrorCode.INVALID_REFRESH_TOKEN));
+
         Claims claims = jwtTokenProvider.validateRefreshToken(refreshToken);
 
         userRepository.findByEmail(claims.getSubject())
             .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
 
         CustomOAuth2User customOAuth2User = jwtTokenProvider.getCustomOAuth2User(claims);
+        TokenResponse.GetToken newTokens = jwtTokenProvider.generateAllTokens(customOAuth2User, new Date());
+        originToken.updateToken(newTokens.refreshToken());
 
-        return jwtTokenProvider.generateAllTokens(customOAuth2User, new Date());
+        return newTokens;
+    }
+
+    public void confirmNickname(UserInfoRequest.ConfirmNickname userInfoRequest) {
+        if (userRepository.existsByNickname(userInfoRequest.nickname())) {
+            throw new IllegalArgumentException(ErrorCode.DUPLICATE_NICKNAME);
+        }
     }
 
     // oauth 회원가입 완료후, 역할 변경
-    public void completeSignup(UserInfoRequest userInfoRequest, String userEmail) {
+    @Transactional
+    public void completeSignup(UserInfoRequest.Signup userInfoRequest, String userEmail) {
         User user = userRepository.findByEmail(userEmail)
             .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
 
         if (!user.getRole().equals(Role.ROLE_GUEST)) {
-            throw new com.example.earthtalk.global.exception.IllegalArgumentException(ErrorCode.EXIST_USER);
+            throw new IllegalArgumentException(ErrorCode.EXIST_USER);
         }
 
-        if (userRepository.existsByNickname(userInfoRequest.nickname())) {
-            throw new com.example.earthtalk.global.exception.IllegalArgumentException(ErrorCode.DUPLICATE_NICKNAME);
-        }
-
-        user.updateNickname(userInfoRequest.nickname());
+        user.updateSignupInfo(userInfoRequest.nickname(), userInfoRequest.introduction());
         user.updateRole(Role.ROLE_MEMBER);
-        userRepository.save(user);
+    }
+
+    //TODO: save 삭제 후 transactional 처리
+    public void saveRefreshToken(CustomOAuth2User oAuth2User, String refreshToken) {
+        String userEmail = oAuth2User.getEmail();
+
+        RefreshToken refreshTokenEntity = refreshTokenRepository.findByUserEmail(userEmail)
+            .orElse(RefreshToken.builder()
+                .userEmail(userEmail)
+                .build());
+
+        refreshTokenEntity.updateToken(refreshToken); // 새로운 토큰 값 설정
+        refreshTokenRepository.save(refreshTokenEntity);
+    }
+
+    /**
+     * SocialType과 attributes에 들어있는 소셜 로그인의 식별값 id를 통해 회원을 찾아 반환하는 메소드<br> 만약 찾은 회원이 있다면, 그대로 반환하고
+     * 없다면 saveUser()를 호출하여 회원을 저장한다.
+     */
+    public User getOrSaveUser(OAuthAttributes attributes, SocialType socialType, TokenResponse.GetToken tokens) {
+        User findUser = userRepository.findBySocialTypeAndSocialId(socialType,
+            attributes.getOauth2UserResponse().getId()).orElse(null);
+
+        if (findUser == null) {
+            return saveUser(attributes, socialType, tokens);
+        }
+        findUser.updateTokens(tokens.accessToken(), tokens.refreshToken());
+        return userRepository.save(findUser);
     }
 
     private ClientRegistration getClientRegistration(String provider) {
         ClientRegistration clientRegistration = ((InMemoryClientRegistrationRepository) clientRegistrationRepository).findByRegistrationId(provider);
         if (clientRegistration == null) {
-            throw new IllegalArgumentException("지원되지 않는 OAuth 타입입니다.");
+            throw new IllegalArgumentException(ErrorCode.INVALID_SOCIAL_TYPE);
         }
         return clientRegistration;
     }
@@ -159,6 +191,12 @@ public class OAuth2Service {
         return customOAuth2User;
     }
 
+    // 닉네임 입력을 안 받았기 때문에 임시 닉네임 설정 후, GUEST User 객체 생성 후 반환
+    private User saveUser(OAuthAttributes attributes, SocialType socialType, TokenResponse.GetToken tokens) {
+        User createdUser = attributes.toEntity(socialType, attributes.getOauth2UserResponse(), tokens);
+        return userRepository.save(createdUser);
+    }
+
     private SocialType getSocialType(String registrationId) {
         if (NAVER.equals(registrationId)) {
             return SocialType.NAVER;
@@ -167,37 +205,5 @@ public class OAuth2Service {
             return SocialType.KAKAO;
         }
         return SocialType.GOOGLE;
-    }
-
-    /**
-     * SocialType과 attributes에 들어있는 소셜 로그인의 식별값 id를 통해 회원을 찾아 반환하는 메소드<br> 만약 찾은 회원이 있다면, 그대로 반환하고
-     * 없다면 saveUser()를 호출하여 회원을 저장한다.
-     */
-    private User getOrSaveUser(OAuthAttributes attributes, SocialType socialType) {
-        User findUser = userRepository.findBySocialTypeAndSocialId(socialType,
-            attributes.getOauth2UserResponse().getId()).orElse(null);
-
-        if (findUser == null) {
-            return saveUser(attributes, socialType);
-        }
-        return findUser;
-    }
-
-    // 닉네임 입력을 안 받았기 때문에 임시 닉네임 설정 후, GUEST User 객체 생성 후 반환
-    private User saveUser(OAuthAttributes attributes, SocialType socialType) {
-        User createdUser = attributes.toEntity(socialType, attributes.getOauth2UserResponse());
-        return userRepository.save(createdUser);
-    }
-
-    private void saveRefreshToken(CustomOAuth2User oAuth2User, String refreshToken) {
-        String userEmail = oAuth2User.getEmail();
-
-        RefreshToken refreshTokenEntity = refreshTokenRepository.findByUserEmail(userEmail)
-            .orElse(RefreshToken.builder()
-                .userEmail(userEmail)
-                .build());
-
-        refreshTokenEntity.updateToken(refreshToken); // 새로운 토큰 값 설정
-        refreshTokenRepository.save(refreshTokenEntity); // 토큰 존재하면 수정, 없으면 저장
     }
 }
