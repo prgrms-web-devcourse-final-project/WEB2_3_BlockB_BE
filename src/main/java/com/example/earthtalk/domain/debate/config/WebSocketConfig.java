5e4fe6492eecfd1f6f9fb3dc55b7baa3fe2ce849
JinWoong <slsl03264@naver.com>
2025-03-06 16:52:29 +0900

    fix: ws에서 jwt 제거

--- src/main/java/com/example/earthtalk/domain/debate/config/WebSocketConfig.java
+++ src/main/java/com/example/earthtalk/domain/debate/config/WebSocketConfig.java
@@ -3,14 +3,12 @@ package com.example.earthtalk.domain.debate.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import com.example.earthtalk.domain.debate.component.RoomIdInterceptor;
-import com.example.earthtalk.domain.debate.component.WebSocketAuthChannelInterceptor;
 import com.example.earthtalk.global.security.util.JwtTokenProvider;
 
 @Slf4j
@@ -18,8 +16,6 @@ import com.example.earthtalk.global.security.util.JwtTokenProvider;
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-	private final JwtTokenProvider jwtTokenProvider;
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -45,9 +41,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		log.info("Registered STOMP endpoint: /room-list");
 	}
 
-	@Override
-	public void configureClientInboundChannel(ChannelRegistration registration) {
-		registration.interceptors(new WebSocketAuthChannelInterceptor(jwtTokenProvider));
-		log.info("Configured client inbound channel with WebSocketAuthChannelInterceptor");
-	}
 }
