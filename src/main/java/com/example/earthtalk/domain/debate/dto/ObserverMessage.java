e39c80c74988f4e5e08a69f7a9d8d1abb5c35332
OCS <ku.ocs.94@gmail.com>
2025-03-10 11:06:23 +0900

    fix: 종료된 토론방 조회 등 API 수정

--- src/main/java/com/example/earthtalk/domain/debate/dto/ObserverMessage.java
+++ src/main/java/com/example/earthtalk/domain/debate/dto/ObserverMessage.java
@@ -3,10 +3,10 @@ package com.example.earthtalk.domain.debate.dto;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ObserverMessage {
