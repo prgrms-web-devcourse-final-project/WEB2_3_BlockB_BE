44f9d173a6f5f2bf60ffb91a60a14ed76cd641dc
박현욱 <nic991002@gmail.com>
2025-03-10 16:49:05 +0900

    fix: 잘못 올라간 커밋 내용 삭제

--- src/main/java/com/example/earthtalk/domain/debate/entity/Debate.java
+++ src/main/java/com/example/earthtalk/domain/debate/entity/Debate.java
@@ -90,7 +90,7 @@ public class Debate extends BaseTimeEntity {
     @Column(nullable = false)
     private Long neutralNumber;
 
-    @Column(nullable = false, columnDefinition = "TINYINT(1)")
+    @Column(nullable = false)
     private boolean resultEnabled;
 
     @Column(nullable = false)
