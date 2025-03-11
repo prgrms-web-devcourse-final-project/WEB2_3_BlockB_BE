fa932b4aa47e5dfaf6fa4d72be121fd54850b119
JinWoong <slsl03264@naver.com>
2025-03-11 12:42:14 +0900

    feat: waitRoom은 redis 에서, 토론방 조회는 db에서 해올 수 있도록 구조 변경

--- src/main/java/com/example/earthtalk/domain/debate/repository/DebateParticipantsRepository.java
+++ src/main/java/com/example/earthtalk/domain/debate/repository/DebateParticipantsRepository.java
@@ -7,10 +7,12 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.earthtalk.domain.debate.entity.DebateParticipants;
+import com.example.earthtalk.domain.debate.entity.FlagType;
 
 public interface DebateParticipantsRepository extends JpaRepository<DebateParticipants, Long> {
 	Optional<DebateParticipants> findByDebate_UuidAndUser_Nickname(UUID debateUuid, String username);
 	List<DebateParticipants> findByDebate_Uuid(UUID debateUuid);
+	List<DebateParticipants> findByDebate_UuidAndPosition(UUID debateUuid, FlagType position);
 
     void deleteAllByUserId(Long id);
 }
